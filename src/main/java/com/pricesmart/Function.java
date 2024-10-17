7913bfec328b57c54ae7e6d3b073c66ae38b2978
Alvaro Lobaton <alobaton@pricesmart.com>
2024-10-17 09:38:25 -0500

    Initial commit

---
+++ src/main/java/com/pricesmart/Function.java
@@ -0,0 +1,23 @@
+package com.pricesmart;
+
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.ServiceBusTopicTrigger;
+import io.quarkus.logging.Log;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class Function {
+
+    @FunctionName("quarkus-data-sync-demo")
+    public String run(
+        @ServiceBusTopicTrigger(name = "message",
+            topicName = "%TOPIC_NAME%",
+            subscriptionName = "%SUBSCRIPTION_NAME%",
+            connection = "ServiceBusConnectionString")
+        String message,
+        final ExecutionContext context) {
+        Log.info("message=" + message);
+        return message;
+    }
+}
